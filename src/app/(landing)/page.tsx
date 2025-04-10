38f010e76cbd2efe5d3198940001169af1d0e0a4
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-04-10 08:20:48 +0300

    resolve code review comments

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -7,16 +7,14 @@ import flyer from '../../../public/images/flyer-web.png'
 export default function Home() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="">
-        <div className="flex size-full items-center justify-center">
-          <Image
-            src={flyer}
-            alt="Picture of chess tornament flyer"
-            priority
-            className="w-10/12 sm:w-2/3 lg:w-4/5"
-            placeholder="blur"
-          />
-        </div>
+      <div className="flex size-full items-center justify-center">
+        <Image
+          src={flyer}
+          alt="Picture of chess tornament flyer"
+          priority
+          className="w-10/12 sm:w-2/3 lg:w-4/5"
+          placeholder="blur"
+        />
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
@@ -25,7 +23,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
     </div>
   )
 }
