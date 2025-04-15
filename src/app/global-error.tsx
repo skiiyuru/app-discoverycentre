394880d65bf6e96ae835aa7b80232b540d1287fc
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-04-15 15:00:02 +0300

    refator disconnect logic

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,6 +3,8 @@
 import * as Sentry from '@sentry/nextjs'
 import { useEffect } from 'react'
 
+import { Button } from '@/components/ui/button'
+
 export default function GlobalError({ error, reset }: { error: Error & { digest?: string }, reset: () => void }) {
   useEffect(() => {
     Sentry.captureException(error)
@@ -12,7 +14,13 @@ export default function GlobalError({ error, reset }: { error: Error & { digest?
     <html>
       <body>
         <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <Button
+          onClick={
+            () => reset()
+          }
+        >
+          Try again
+        </Button>
       </body>
     </html>
   )
